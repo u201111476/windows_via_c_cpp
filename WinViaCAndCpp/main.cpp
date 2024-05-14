8d0e33b0cf705f0355aa50d20ecb5227f5cc1768
weijun <weij13@chinatelecom.cn>
2024-05-14 18:19:37 +0800

    feat:添加pdh

--- WinViaCAndCpp/main.cpp
+++ WinViaCAndCpp/main.cpp
@@ -1,8 +1,19 @@
 #include <Windows.h>
 #include <stdio.h>
 #include <assert.h>
+#include <ShlObj.h>
+#include <Pdh.h>
+#include <iostream>
+
+#include <IPHlpApi.h>
+#pragma comment(lib, "IPHlpApi.lib")
+#pragma comment(lib,"pdh.lib")
+
+using namespace std;
 
 extern "C" const IMAGE_DOS_HEADER __ImageBase;			//这个变量的地址就是hInstance，也就是exe文件的加载地址
+
+HANDLE hin, hout;
 
 DWORD WINAPI ThreadFunc(LPVOID lpParam)
 {
@@ -12,11 +23,201 @@ DWORD WINAPI ThreadFunc(LPVOID lpParam)
 	}
 }
 
+BOOL GetProcessElevation(TOKEN_ELEVATION_TYPE* type, BOOL* bIsAdmin)
+{
+	//HANDLE tokenHandle;
+	//if(!OpenProcessToken(GetCurrentProcess(), TOKEN_QUERY, &tokenHandle))
+	//	return FALSE;
+	//DWORD size;
+	//if (GetTokenInformation(tokenHandle, TokenElevationType, type, sizeof(TOKEN_ELEVATION_TYPE), &size))
+	//{
+	//	BOOL b{TRUE};
+	//	SID_IDENTIFIER_AUTHORITY NtAuthority = SECURITY_NT_AUTHORITY;
+	//	PSID AdministratorsGroup;
+	//	b = AllocateAndInitializeSid(
+	//		&NtAuthority,
+	//		2,
+	//		SECURITY_BUILTIN_DOMAIN_RID,
+	//		DOMAIN_ALIAS_RID_ADMINS,
+	//		0, 0, 0, 0, 0, 0,
+	//		&AdministratorsGroup);
+	//	if (b)
+	//	{
+	//		if (!CheckTokenMembership(NULL, AdministratorsGroup, &b))
+	//		{
+	//			b = FALSE;
+	//		}
+	//		FreeSid(AdministratorsGroup);
+	//	}
+	//	*bIsAdmin = b;
+	//}
+	//return TRUE;
+
+
+	//书上的源码执行有问题，无论是不是管理员权限打开，都会判断成是管理员权限
+	//把TokenLinkedToken去掉，然后CheckTokenMembership第一个参数设置为NULL，就行了
+	HANDLE hToken = NULL;
+	DWORD dwSize;
+
+	// Get current process token
+	if (!OpenProcessToken(GetCurrentProcess(), TOKEN_QUERY, &hToken))
+		return(FALSE);
+
+	BOOL bResult = FALSE;
+
+	// Retrieve elevation type information 
+	if (GetTokenInformation(hToken, TokenElevationType,
+		type, sizeof(TOKEN_ELEVATION_TYPE), &dwSize)) {
+		// Create the SID corresponding to the Administrators group
+		byte adminSID[SECURITY_MAX_SID_SIZE];
+		dwSize = sizeof(adminSID);
+		CreateWellKnownSid(WinBuiltinAdministratorsSid, NULL, &adminSID,
+			&dwSize);
+
+		if (*type == TokenElevationTypeLimited) {
+			// Get handle to linked token (will have one if we are lua)
+			// //把这个去掉就行了
+			//HANDLE hUnfilteredToken = NULL;
+			//GetTokenInformation(hToken, TokenLinkedToken, (VOID*)
+			//	&hUnfilteredToken, sizeof(HANDLE), &dwSize);
+
+			// Check if this original token contains admin SID
+			if (CheckTokenMembership(NULL, &adminSID, bIsAdmin)) {
+				bResult = TRUE;
+			}
+
+			// Don't forget to close the unfiltered token
+			//CloseHandle(hUnfilteredToken);
+		}
+		else {
+			*bIsAdmin = IsUserAnAdmin();
+			bResult = TRUE;
+		}
+	}
+
+	// Don't forget to close the process token
+	CloseHandle(hToken);
+
+	return(bResult);
+}
+
+
+string getNetInterface() {
+	ULONG ulSize = 0;
+	IP_ADAPTER_INFO* pAdapter = nullptr;
+	if (GetAdaptersInfo(pAdapter, &ulSize) == ERROR_BUFFER_OVERFLOW) {
+		pAdapter = (IP_ADAPTER_INFO*)new char[ulSize];
+	}
+	else {
+		cout << "GetAdaptersInfo fail" << endl;
+		return "";
+	}
+
+	if (GetAdaptersInfo(pAdapter, &ulSize) != ERROR_SUCCESS) {
+		cout << "GetAdaptersInfo fail" << endl;
+		return "";
+	}
+
+	IPAddr ipAddr = { 0 };
+	DWORD dwIndex = -1;
+	DWORD nRet = GetBestInterface(ipAddr, &dwIndex);
+	if (NO_ERROR != nRet) {
+		cout << "GetBestInterface fail: " << nRet << endl;
+	}
+
+	string strInterface;
+	for (auto* pCur = pAdapter; pCur != NULL; pCur = pCur->Next) {
+		//if (pCur->Type != MIB_IF_TYPE_ETHERNET)
+		//  continue;
+
+		if (pCur->Index == dwIndex) {
+			cout << "Best Interface!! ";
+			strInterface = pCur->Description;
+		}
+
+		cout << "Descrip: " << pCur->Description;
+		cout << ", Name: " << pCur->AdapterName << endl;
+		cout << "IP: " << pCur->IpAddressList.IpAddress.String;
+		cout << ", Gateway: " << pCur->GatewayList.IpAddress.String << endl << endl;
+	}
+
+	delete pAdapter;
+	return strInterface;
+}
+
+//仅提供参考，表示可以通过PDH获取性能数据
+void getResourceCounter()
+{
+	HQUERY query;
+	PDH_STATUS status = PdhOpenQuery(NULL, NULL, &query);
+	if (status != ERROR_SUCCESS)
+		cout << "Open Query Error" << endl;
+
+	HCOUNTER cpuCounter, memCounter;
+	HCOUNTER recvCounter, sentCounter;
+
+	//string strGet = getNetInterface();
+	//wstring strInterface = L"\\Network Interface(" + xugd::clib::XuStr::str2wstr(strGet) + L")\\";
+	//wcout << strInterface << endl;
+
+	status = PdhAddCounter(query, TEXT("\\Processor Information(_Total)\\% Processor Utility"), NULL, &cpuCounter);			//对应任务管理器的CPU利用率
+	if (status != ERROR_SUCCESS)
+		cout << "Add CPU Counter Error" << endl;
+	status = PdhAddCounter(query, TEXT("\\Memory\\Available MBytes"), NULL, &memCounter);
+	if (status != ERROR_SUCCESS)
+		cout << "Add Memory Counter Error" << endl;
+
+	//status = PdhAddCounter(query, (strInterface + L"Bytes Received/sec").c_str(), NULL, &recvCounter);
+	//if (status != ERROR_SUCCESS)
+	//	cout << "Add Received Counter Error" << endl;
+	//status = PdhAddCounter(query, (strInterface + L"Bytes Sent/sec").c_str(), NULL, &sentCounter);
+	//if (status != ERROR_SUCCESS)
+	//	cout << "Add Sent Counter Error" << endl;
+
+
+	int nIndex = 0;
+	//cout << setiosflags(ios::fixed) << setprecision(4);
+	char buf[256] = { 0 };
+	while (true) {
+		PdhCollectQueryData(query);
+		Sleep(1000);
+
+		PdhCollectQueryData(query);
+
+		PDH_FMT_COUNTERVALUE pdhValue;
+		DWORD dwValue;
+		ZeroMemory(buf, 256);
+		status = PdhGetFormattedCounterValue(cpuCounter, PDH_FMT_DOUBLE, &dwValue, &pdhValue);
+		/*if (status != ERROR_SUCCESS)
+			cout << "Get Value Error" << endl;*/
+		//cout << setw(3) << ++nIndex << " - CPU: " << pdhValue.doubleValue << "%";
+		sprintf_s(buf, 255, "CPU:%2.2f%%\n", pdhValue.doubleValue);
+		WriteConsole(hout, buf, strlen(buf), NULL, NULL);
+		status = PdhGetFormattedCounterValue(memCounter, PDH_FMT_LONG, &dwValue, &pdhValue);
+		//if (status != ERROR_SUCCESS)
+		//	cout << "Get Value Error" << endl;
+		//cout << "; \tMemory: " << pdhValue.longValue << "MB";
+
+		//status = PdhGetFormattedCounterValue(recvCounter, PDH_FMT_LONG, &dwValue, &pdhValue);
+		//if (status != ERROR_SUCCESS)
+		//	cout << "Get Value Error" << endl;
+		//cout << "; \tRecv: " << pdhValue.longValue;
+		//status = PdhGetFormattedCounterValue(sentCounter, PDH_FMT_LONG, &dwValue, &pdhValue);
+		//if (status != ERROR_SUCCESS)
+		//	cout << "Get Value Error" << endl;
+		//cout << "; \tSent: " << pdhValue.longValue << endl;
+
+		//Sleep(1000);
+	}
+
+	PdhCloseQuery(query);
+}
+
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE, LPSTR lpCmdLine, int nCmdShow)
 {
 	auto bAllocConsole = AllocConsole();		//一个进程只能由一个控制台
-	HANDLE hin = GetStdHandle(STD_INPUT_HANDLE);		//获取标准输入的句柄，不需要手动Close
-	HANDLE hout = GetStdHandle(STD_OUTPUT_HANDLE);		//获取标准输出的句柄，不需要手动Close
+	hin = GetStdHandle(STD_INPUT_HANDLE);		//获取标准输入的句柄，不需要手动Close
+	hout = GetStdHandle(STD_OUTPUT_HANDLE);		//获取标准输出的句柄，不需要手动Close
 	char buf[256] = { 0 };
 	char buf2[1024] = { 0 };
 	sprintf_s(buf, 255,"hInstance:0x%x,&__ImageBase:0x%x\n", hInstance, &__ImageBase);			//这两者是相同的
@@ -94,15 +295,38 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE, LPSTR lpCmdLine, int nCmdShow
 	OSVERSIONINFO ver;
 	ver.dwOSVersionInfoSize = sizeof(ver);
 	//GetVersionEx(&ver);			//被声明为已否决
+	bool bIsAdmin = IsUserAnAdmin();
+	ZeroMemory(buf, 256);
+	if (bIsAdmin)
+		sprintf_s(buf, 255,"User is Admin!\n");
+	else
+		sprintf_s(buf, 255, "User is not Admin!\n");
+	WriteConsole(hout, buf, strlen(buf), NULL, NULL);
+	TOKEN_ELEVATION_TYPE teType;
+	BOOL isAdmin;
+	GetProcessElevation(&teType, &isAdmin);
+	sprintf_s(buf, 255, "TOKEN_ELEVATION_TYPE is:%d,isAdmin is:%d\n", teType, isAdmin);
+	WriteConsole(hout, buf, strlen(buf), NULL, NULL);
+	//getResourceCounter();
 
-	//while (true)
+	//PDH_HQUERY query;
+	//if (PdhOpenQuery(NULL, NULL, &query) == ERROR_SUCCESS)
 	//{
-	//	Sleep(5000);
+	//	PdhAddCounter(query,"Processor\\ % Total Processor Time")
 	//}
 
+
 	CreateThread(NULL, NULL, ThreadFunc, NULL, 0, NULL);
-	FreeConsole();
 	//ExitThread(1);			//不调用ExitThread的话，主线程return了，会调用exit,先清理一些静态/全局的C++对象，然后调用ExitProcess,整个进程就会退出
-								//调用了ExitThread的话，由于不走C/C++运行时那套退出机制了，而Windows系统保证一个进程只有当所有线程退出了之后才会退出，所以上面创建的线程还会继续执行，进程不退出
+	//调用了ExitThread的话，由于不走C/C++运行时那套退出机制了，而Windows系统保证一个进程只有当所有线程退出了之后才会退出，所以上面创建的线程还会继续执行，进程不退出
+	while (true)
+	{
+		ZeroMemory(buf, 256);
+		DWORD nRead;
+		ReadConsole(hin, buf, 256, &nRead, NULL);
+		if (strncmp(buf,"exit",4) == 0)
+			break;
+	}
+	FreeConsole();
 	return 0;
 }
